9645c5a2dc363613a08d9c4b2a169b7bbf199243
apereira123 <a.j.pereira13@hotmail.com>
2016-04-30 15:33:50 -0400

    Commit

--- src/personal/PigGameLogic.java
+++ src/personal/PigGameLogic.java
@@ -17,30 +17,45 @@ public class PigGameLogic {
 		tempUserSum = 0;
 		while(s.equals("y") && stillPlaying == true && userSum < 100) {
 			userRoll();
-			if (dice.getDie1() != 1 && dice.getDie2() != 1) {
+			if (dice.getDie1() != 1 && dice.getDie2() != 1 && userSum < 100) {
 				System.out.println("If you would like to roll again enter \"y\".");
 				s = scan.nextLine();
 			}
 		}
  		stillPlaying = true;
- 		System.out.println("Your sum is " + userSum + ".");
- 		System.out.println();
+ 		if (userSum < 100) {
+ 			System.out.println("Your sum is " + userSum + ".");
+ 	 		System.out.println();
+ 	 		try {
+ 			    Thread.sleep(2000);
+ 			} catch(InterruptedException ex) {
+ 			    Thread.currentThread().interrupt();
+ 			}
+ 		}
 	}
 	
 	public static void compTurn() {
 		tempCompSum = 0;
 		while(tempCompSum < 20 && stillPlaying == true && compSum < 100) {
 			compRoll();
-			try {
-			    Thread.sleep(1000);
-			} catch(InterruptedException ex) {
-			    Thread.currentThread().interrupt();
+			if (compSum < 100) {
+				try {
+				    Thread.sleep(2000);
+				} catch(InterruptedException ex) {
+				    Thread.currentThread().interrupt();
+				}
 			}
 		}
 		stillPlaying = true;
-		System.out.println("The computer's sum is " + compSum + ".");
-		System.out.println();
-		
+		if (compSum < 100) {
+ 			System.out.println("The computer's sum is " + compSum + ".");
+ 	 		System.out.println();
+ 	 		try {
+ 			    Thread.sleep(2000);
+ 			} catch(InterruptedException ex) {
+ 			    Thread.currentThread().interrupt();
+ 			}
+ 		}
 	}
 	
 	public static void userRoll() {
@@ -48,11 +63,11 @@ public class PigGameLogic {
 		if (dice.getDie1() == 1 && dice.getDie2() == 1) {
 			userSum = 0;
 			stillPlaying = false;
-			System.out.println("You rolled two ones. You lose all point earned thus far and your turn is over.");
+			System.out.println("You rolled two 1s. You lose all point earned thus far and your turn is over.");
 		} else if (dice.getDie1() == 1 || dice.getDie2() == 1) {
 			userSum -= tempUserSum;
 			stillPlaying = false;
-			System.out.println("You rolled a one. You lose all point for this round and your turn is over.");
+			System.out.println("You rolled a 1. You lose all point for this round and your turn is over.");
 		} else {
 			if (dice.getSum() == 8 || dice.getSum() == 11) {
 				System.out.println("You rolled an " + dice + ".");
@@ -69,11 +84,11 @@ public class PigGameLogic {
 		if (dice.getDie1() == 1 && dice.getDie2() == 1) {
 			compSum = 0;
 			stillPlaying = false;
-			System.out.println("The computer rolled two ones. It loses all point earned thus far and its turn is over.");
+			System.out.println("The computer rolled two 1s. It loses all point earned thus far and its turn is over.");
 		} else if (dice.getDie1() == 1 || dice.getDie2() == 1) {
 			compSum -= tempCompSum;
 			stillPlaying = false;
-			System.out.println("The computer rolled a one. It loses all point for this round and its turn is over.");
+			System.out.println("The computer rolled a 1. It loses all point for this round and its turn is over.");
 		} else {
 			if (dice.getSum() == 8 || dice.getSum() == 11) {
 				System.out.println("The computer rolled an " + dice + ".");
--- src/personal/PigGameMain.java
+++ src/personal/PigGameMain.java
@@ -5,6 +5,12 @@ public class PigGameMain extends PigGameLogic {
 	public static void main(String[] args) {
 		
 		System.out.println("I hope you enjoy the game. Good luck and have fun!");
+		System.out.println();
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
 		while (userSum < 100 && compSum < 100) {
 			userTurn();
 			if (userSum < 100) {
@@ -12,8 +18,10 @@ public class PigGameMain extends PigGameLogic {
 			}
 		}
 		if (userSum >= 100) {
+			System.out.println();
 			System.out.println("Congratulations, you won! Your sum is " + userSum + " .");
 		} else {
+			System.out.println();
 			System.out.println("Sorry, the computer won. Its sum is " + compSum + ".");
 		}
 		System.out.println("Thank you for playing.");
